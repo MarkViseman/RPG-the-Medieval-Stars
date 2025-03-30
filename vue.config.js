548b92291a586c6fef80ff3a3a4c350e09f725f3
Mik Wiseman <hi@mikwiseman.com>
2025-03-30 10:41:57 +0300

    Update build configuration for Vercel deployment

--- vue.config.js
+++ vue.config.js
@@ -5,27 +5,9 @@ module.exports = defineConfig({
   publicPath: '/',
   productionSourceMap: false,
   configureWebpack: {
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 244000,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-        },
-      },
-    },
-  },
+    output: {
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].js'
+    }
+  }
 }) 
